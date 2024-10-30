2d90fdbce204c0b769a20ca71c40283c69b741da
Sayori-h <sayorihu0618@gmail.com>
2024-10-30 17:31:12 +0800

    1.论文添加

--- ILStools/transKml.py
+++ ILStools/transKml.py
@@ -5,7 +5,7 @@ file_path = '../doc/kml.xlsx'  # Update with your actual path
 data = pd.read_excel(file_path)
 
 # Format the data as "latitude,longitude,altitude" without quotes
-formatted_data = data.apply(lambda row: f"{row['LON']},{row['LAT']},{int(row['ALT'])}", axis=1)
+formatted_data = data.apply(lambda row: f"{row['LON']},{row['LAT']},{int(row['ALT'])*0.3048}", axis=1)
 
 # Save the formatted data to a text file
 output_path = '../doc/converted_coordinates.txt'  # Update with your actual path
